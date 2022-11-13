751261a7a0d5595d0a2ee9dbc8154a749fb4b9df
Adam Hemming <hemming.dev@gmail.com>
2022-11-13 22:45:20 +0900

    make add 100 function

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-random-practice
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/clojure_random_practice/core.clj
@@ -0,0 +1,13 @@
+(ns clojure-random-practice.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Random practiec!"))
+
+(defn add-100
+  [num]
+  (+ num 100))
+
+(println (add-100 5))
